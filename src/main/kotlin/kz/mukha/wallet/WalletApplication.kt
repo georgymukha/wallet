4f5c96bd7d2194f9526085226f4bb0bf47ce907c
George Mukha <georgymukha@yandex.ru>
2024-05-12 12:43:26 +0500

    Init repo

---
+++ src/main/kotlin/kz/mukha/wallet/WalletApplication.kt
@@ -0,0 +1,11 @@
+package kz.mukha.wallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalletApplication
+
+fun main(args: Array<String>) {
+	runApplication<WalletApplication>(*args)
+}
